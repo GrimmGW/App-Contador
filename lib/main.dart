c7c9e98ddd9ff893b315ec5759e8d8ab8c792be6
Hector Luna <hectorl1805@gmail.com>
2025-05-29 18:14:50 -0400

    Optimizando código y agregando modo oscuro

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: const HomeScreen(),
+      theme: ThemeData.dark(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,36 +7,63 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-int counter = 0;
-
 class _HomeScreenState extends State<HomeScreen> {
+  int counter = 0;
+
+  void increase() {
+    counter++;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
+    const textStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 26);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Primera App - Flutter'),
         backgroundColor: Colors.redAccent,
+        actions: [const Icon(Icons.search)],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('El valor actual es:'),
-            Text(
-              '$counter',
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
-            ),
+            Text('$counter', style: textStyle),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            counter++;
-          });
-        },
-        child: const Icon(Icons.plus_one),
-      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: CustomFloatingActionButton(increaseFn: increase),
+    );
+  }
+}
+
+class CustomFloatingActionButton extends StatelessWidget {
+  final Function increaseFn;
+
+  const CustomFloatingActionButton({super.key, required this.increaseFn});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FloatingActionButton(
+          backgroundColor: Colors.redAccent,
+          foregroundColor: Colors.black,
+          onPressed: () => increaseFn(),
+          child: const Icon(Icons.plus_one),
+        ),
+        const SizedBox(width: 30),
+        FloatingActionButton(
+          backgroundColor: Colors.redAccent,
+          foregroundColor: Colors.black,
+          onPressed: () {},
+          child: const Icon(Icons.exposure_minus_1),
+        ),
+      ],
     );
   }
 }
